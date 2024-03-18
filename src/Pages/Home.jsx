c82790c4774dc419b6fd833361cde1dcb6c9fcab
MrAplack <feltronix@gmail.com>
2024-03-17 21:48:51 -0300

    clase7

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,9 +2,20 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
-    const [task, setTasks] = useState([])
+    const [tasks, setTasks] = useState([])
+    const [task, setTask] = useState(null)
+    const [responsable, setResponsable] = useState("")
+    const [description, setDescription] = useState("")
+    const [error, setError] = useState("")
 
-    const getTask = async () => {
+
+    const refreshFormulario = async () => {
+
+        setResponsable("")
+        setDescription("")
+
+    }
+    const getTasks = async () => {
 
         const response = await fetch("https://api-tasks.vercel.app/api/tasks")
 
@@ -15,33 +26,89 @@ export default function Home() {
 
     }
 
+    const getTaskById = async (id) => {
+
+        const response = await fetch(`https://api-tasks.vercel.app/api/task/${id}`)
+
+        const data = await response.json()
+
+        setTask(data)
+
+
+    }
+
+    const deleteTaskById = async (id) => {
+
+        const response = await fetch(`https://api-tasks.vercel.app/api/task/${id}`, { method: "delete" })
+
+
+
+        const data = await response.json()
+
+        setTask(null)
+
+        await getTasks()
+
+
+
+    }
+
+    const updateTaskById = async (id) => {
+
+        const response = await fetch(`https://api-tasks.vercel.app/api/task/${id}`, {
+            method: "PATCH",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+
+                responsable: "Pedro",
+                description: "Es Otra Tarea de Javier"
+
+            })
+        })
+
+        const data = await response.json()
+
+        await getTasks()
+
+
+
+    }
+
+    const validateForm = () =>{
+
+        if(responsable === "") setError("Responsable no puede ser vacio")
+        else if(description === "") setError("Tarea no puede estar vacia")
+        else setError("")
+    }
+
     const createTask = async () => {
 
         const response = await fetch("https://api-tasks.vercel.app/api/task", {
 
-        method: "post",
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            
-            responsable: "Pedro",
-            description: "Es Otra Tarea de Javier"
-            
-        })
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+
+                responsable: responsable,
+                description: description
+
+            })
 
         })
 
         const data = await response.json()
-        await getTask()
+        await getTasks()
         console.log(data)
+        refreshFormulario()
 
 
     }
 
     useEffect(() => {
 
-        getTask()
+        getTasks()
 
 
     }, [])
@@ -50,17 +117,33 @@ export default function Home() {
     return <div>
 
         <h1>Home desde componente</h1>
+        <div>
+            <h2>Formulario de Cracion</h2>
+            <input type="text" value= {responsable} onChange={(event)=> setResponsable (event.target.value)} placeholder="Responsable"></input>
+            <input type="text" value= {description} onChange={(event)=> setDescription (event.target.value)} placeholder="Descripcion"></input>
+        </div>
+
+
+
+
+
         <button onClick={() => createTask()}>Crear Tarea</button>
         {
 
-            task.map((task) => {
+            tasks.map((task) => {
 
 
                 return <div key={task.id}>
 
-                    <div>Responsable: {task.responsable}</div>
-                    <div>Descripcion: {task.description}</div>
+                    <div>Responsable: {task?.responsable}</div>
+                    <div>Descripcion: {task?.description}</div>
                     <hr />
+
+                    <div>
+
+                        <button onClick={() => deleteTaskById(task.id)}>Borrar Tarea</button>
+
+                    </div>
 
                 </div>
 
